6a4ba114cf4763ca7300a6342ac8f6b0fc317ee0
Damodar Lohani <dlohani48@gmail.com>
2022-08-17 01:41:35 +0000

    fix undefined env

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -1,6 +1,6 @@
 export let baseUrl = process.env.APP_BASE_URL
 
 export const baseUrlMiddleware = (req, res, next) => {
-    baseUrl = baseUrl.length > 0 ? baseUrl : req.protocol + "://" + req.hostname
+    baseUrl = (baseUrl && baseUrl.length > 0) ? baseUrl : req.protocol + "://" + req.hostname
     next()
 }
