d63eff19337775c0ac656c4ac635739e15dba9de
Damodar Lohani <dlohani48@gmail.com>
2022-08-18 04:51:08 +0000

    copyright notice

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
-    import Header from '$lib/components/Header.svelte';
-import ToastContainer from '$lib/components/ToastContainer.svelte';
-    import '../app.css'
+	import Header from '$lib/components/Header.svelte';
+	import ToastContainer from '$lib/components/ToastContainer.svelte';
+	import '../app.css';
 </script>
+
 <svelte:head>
-    <title>Best URL Shortner</title>
+	<title>Best URL Shortner</title>
 </svelte:head>
 <Header />
-<slot></slot>
+<slot />
 <ToastContainer />
+<div class="copyright">
+	Copyright &copy; <a href="https://dlohani.com.np" target="_blank">Damodar Lohani</a>
+</div>
+
+<style>
+    div.copyright {
+        position: fixed;
+        background-color: #111;
+        color: #fff;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 10px 50px;
+        bottom: 0;
+    }
+</style>
